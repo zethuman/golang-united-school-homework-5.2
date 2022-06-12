aab0e6912960515f2fd44ccfc7370393c3bbe0f3
Rakhat <zhumanrakhat01@gmail.com>
2022-06-12 12:04:06 +0600

    attempt 1

--- cache.go
+++ cache.go
@@ -1,23 +1,55 @@
 package cache
 
-import "time"
+import (
+	"time"
+)
 
 type Cache struct {
+	db map[string]struct {
+		value    string
+		deadline time.Time
+	}
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{db: make(map[string]struct {
+		value    string
+		deadline time.Time
+	})}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (receiver Cache) Get(key string) (string, bool) {
+	entry, ok := receiver.db[key]
+	if !entry.deadline.IsZero() && entry.deadline.Before(time.Now()) {
+		ok = false
+		entry.value = ""
+	}
+	return entry.value, ok
 }
 
-func (receiver) Put(key, value string) {
+func (receiver *Cache) Put(key, value string) {
+	receiver.db[key] = struct {
+		value    string
+		deadline time.Time
+	}{value: value}
 }
 
-func (receiver) Keys() []string {
+func (receiver Cache) Keys() []string {
+	keys := make([]string, len(receiver.db))
+
+	for k := range receiver.db {
+		if !receiver.db[k].deadline.IsZero() && receiver.db[k].deadline.Before(time.Now()) {
+			continue
+		}
+		keys = append(keys, k)
+	}
+
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (receiver *Cache) PutTill(key, value string, deadline time.Time) {
+	receiver.db[key] = struct {
+		value    string
+		deadline time.Time
+	}{value: value, deadline: deadline}
 }
